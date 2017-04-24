89596c0c3669663a4f9a0055f3726adeaf601f22
Marlon Baptista de Quadros <merlonrs@gmail.com>
2017-04-24 01:09:26 -0300

    - Aula 78

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted) { 
+            // Lock the ball relative to the paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            // Wait for click mouse action 
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Mouse clicked, Ball Launched");
+
+                Rigidbody2D myRigidbody = GetComponent<Rigidbody2D>();
+                hasStarted = true;
+                myRigidbody.velocity = new Vector2(2f, 10f);
+
+            }
+        }
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -12,13 +12,12 @@ public class Paddle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-
         Vector3 paddlerPos = new Vector3(0.5f, this.transform.position.y, 0f);
 
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 
-        paddlerPos.x = mousePosInBlocks;
+        // https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+        paddlerPos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
 
         this.transform.position = paddlerPos;
 	}
